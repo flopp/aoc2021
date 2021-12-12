fc079f3e5e17fa28b9ba3c50289f359bf88610cb
Florian Pigorsch <fpigorsch@gmail.com>
2021-12-12 12:03:29 +0100

    refactor

--- day12/main.go
+++ day12/main.go
@@ -8,42 +8,46 @@ import (
 	"github.com/flopp/aoc2021/helpers"
 )
 
-func search(node string, edges *map[string][]string, visited *map[string]int, allowSecondVisit bool) int {
-	if node == "end" {
+func isSmall(cave string) bool {
+	return unicode.IsLower(rune(cave[0]))
+}
+
+func findPaths(cave string, connections map[string][]string, visits map[string]int, allowSecondVisit bool) int {
+	if cave == "end" {
 		return 1
 	}
 
 	paths := 0
 
-	(*visited)[node]++
-	for _, next := range (*edges)[node] {
-		if unicode.IsLower(rune(next[0])) {
-			if (*visited)[next] == 0 {
-				paths += search(next, edges, visited, allowSecondVisit)
-			} else if allowSecondVisit && (*visited)[next] == 1 && next != "start" {
-				paths += search(next, edges, visited, false)
+	visits[cave]++
+	for _, nextCave := range connections[cave] {
+		if isSmall(nextCave) {
+			switch visits[nextCave] {
+			case 0:
+				paths += findPaths(nextCave, connections, visits, allowSecondVisit)
+			case 1:
+				if allowSecondVisit && nextCave != "start" {
+					paths += findPaths(nextCave, connections, visits, false)
+				}
 			}
 		} else {
-			paths += search(next, edges, visited, allowSecondVisit)
+			paths += findPaths(nextCave, connections, visits, allowSecondVisit)
 		}
 	}
-	(*visited)[node]--
+	visits[cave]--
 
 	return paths
 }
 
 func main() {
-	edges := make(map[string][]string)
+	connections := make(map[string][]string)
 	helpers.ReadStdin(func(line string) {
 		a := strings.Split(line, "-")
-		edges[a[0]] = append(edges[a[0]], a[1])
-		edges[a[1]] = append(edges[a[1]], a[0])
+		connections[a[0]] = append(connections[a[0]], a[1])
+		connections[a[1]] = append(connections[a[1]], a[0])
 	})
 
-	visited := make(map[string]int)
-	if helpers.Part1() {
-		fmt.Println(search("start", &edges, &visited, false))
-	} else {
-		fmt.Println(search("start", &edges, &visited, true))
-	}
+	visits := make(map[string]int)
+	allowSecondVisit := !helpers.Part1()
+	fmt.Println(findPaths("start", connections, visits, allowSecondVisit))
 }
